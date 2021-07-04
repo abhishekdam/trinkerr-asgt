15d4b1631121b640265bb5b0d9d12b78d3245b8d
Abhishek Dam <abhishekdam852@gmail.com>
2021-07-04 23:43:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome this is fresh start</h1>
+      <h4>
+        I have left the App.css untouched just to make this whole welcome text
+        look good
+      </h4>
+    </div>
+  );
+}
+
+export default App;
