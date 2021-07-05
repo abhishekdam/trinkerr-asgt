65cd322f907d954f4f905f54441054d5f9896b20
Abhishek Dam <abhishekdam852@gmail.com>
2021-07-05 13:24:08 +0530

    login page done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome this is fresh start</h1>
-      <h4>
-        I have left the App.css untouched just to make this whole welcome text
-        look good
-      </h4>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/SignIn">
+          <SignIn />
+        </Route>
+        <Route path="/">
+          <SignUp />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/SignIn.jsx
@@ -0,0 +1,148 @@
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockIcon from '@material-ui/icons/Lock';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { Redirect } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+const SignIn = () => {
+
+    const [getOTP, setOTP] = useState(false);
+    const [phone, setPhone] = useState('');
+    const [otp, setOtp] = useState('');
+    const [flagHandler, setFlagHandler] = useState(false);
+    const [name, setName] = useState('');
+    const [getName, setGetName] = useState(false);
+
+    let flag = 0;
+    const onClick = () => {
+        if (flag == 0) {
+            if (phone) {
+                console.log(phone, "true");
+                setOTP(true)
+                flag = 1
+            }
+            else {
+                console.log(phone, "false");
+                setOTP(false)
+            }
+        }
+        else {
+            if (otp == '0000') {
+                setFlagHandler(true)
+            }
+        }
+    }
+
+    const classes = useStyles();
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} >
+                            <TextField
+                                className="mobileNum"
+                                name="mobileNumber"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="mobileNumber"
+                                label="Mobile Number"
+                                autoFocus
+                                onChange={(e) => { setPhone(e.target.value) }}
+                            />
+                        </Grid>
+                        {
+                            getOTP ?
+                                <Grid item xs={12} >
+                                    <TextField
+                                        name="otp"
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        id="otp"
+                                        label="Enter OTP"
+                                        onChange={(e) => { setOtp(e.target.value) }}
+                                    />
+                                </Grid> : null
+                        }
+                        {
+                            getName ?
+                                <Grid item xs={12} >
+                                    <TextField
+                                        autoComplete="fname"
+                                        name="name"
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        id="name"
+                                        label="Name"
+                                        autoFocus
+                                        onChange={(e) => { setName(e.target.value) }}
+                                    />
+                                </Grid> : null
+                        }
+
+
+                    </Grid>
+
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onClick={onClick}
+                    >
+                        Get OTP
+                    </Button>
+
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                            <Link href="/" variant="body2">
+                                Sign up
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+            {
+                flagHandler && <Redirect to="/home"></Redirect>
+            }
+        </Container>
+    );
+}
+
+export default SignIn
